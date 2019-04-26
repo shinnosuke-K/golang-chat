5839b80a4e3ec7ae4deea910bb2e2f365fb68189
shinnosuke-K <shin.050195@gmail.com>
2019-04-26 15:19:13 +0900

    section: 1.3.1

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -19,17 +20,22 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.templ =
 			template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse() // フラグを解釈
 	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat"})
 	http.Handle("/room", r)
 
+	// チャットルームを開始します
 	go r.run()
 
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	// Webサーバーを起動します
+	log.Println("Webサーバーを開始します。ポート: ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
