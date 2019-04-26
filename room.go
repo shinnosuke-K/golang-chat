9cbb531e63064803597ede40ab80336d8edcf5d8
shinnosuke-K <shin.050195@gmail.com>
2019-04-26 12:46:13 +0900

    section: 1.2.3

--- room.go
+++ room.go
@@ -1,6 +1,7 @@
 package main
 
 type room struct {
+	// 転送するためのメッセージを保持
 	forward chan []byte
 	// チャットの参加
 	join chan *client
@@ -8,4 +9,28 @@ type room struct {
 	leave chan *client
 	// 在室しているクライアント一覧
 	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			// 参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			// 退室
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// すべてのクライアントにメッセージ転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+				default:
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
 }
