691243d424debcd5e7f513ae6f0a2b8a783ba7bd
shinnosuke-K <shin.050195@gmail.com>
2019-04-30 02:00:17 +0900

    section 2.3

--- auth.go
+++ auth.go
@@ -1,6 +1,11 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
 
 type authHandler struct {
 	next http.Handler
@@ -17,6 +22,21 @@ func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	} else {
 		// 成功。ラップされたハンドラを呼び出します
 		h.next.ServeHTTP(w, r)
+	}
+}
+
+// loginHandlerはサードパーティーへのログインの処理を受け付けます
+// パスの形状: /auth/{action}/{provider}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	segs := strings.Split(r.URL.Path, "/")
+	action := segs[2]
+	provider := segs[3]
+	switch action {
+	case "login":
+		log.Println("TODO: ログイン処理", provider)
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "アクション%sには非対応です", action)
 	}
 }
 
--- main.go
+++ main.go
@@ -32,6 +32,7 @@ func main() {
 	//r.tracer = trace.New(os.Stdout)
 	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat"}))
 	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
 	http.Handle("/room", r)
 
 	// Bootstrapをダウンロードする場合
